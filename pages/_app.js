b00c18798dd5540968489d97f118623bd10d6d4d
Sevinchik <sevinch@MacBook-Pro-Shavarsh.local>
2023-08-04 21:08:54 +0300

    start Loading page

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
 import "../styles/globals.scss";
+import React, { useState, useEffect } from "react";
+import Loading from "../components/loading";
 
 function MyApp({ Component, pageProps }) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setIsLoading(false);
+  //   }, 20000);
+  // }, []);
+
   return (
     <main>
-      <Component {...pageProps} />
+      {isLoading ? <Loading></Loading> : <Component {...pageProps} />}
     </main>
   );
 }
